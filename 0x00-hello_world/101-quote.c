31b9d5b33bab35d5408dd9e2bb9cb38c1687a707
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-15 16:14:06 -0700

    hello c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - an entry for the program that prints line to standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
