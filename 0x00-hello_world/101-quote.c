0e9a1cd0b475c2a77c0a9cc8f487101f0e3641ae
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-15 16:28:12 -0700

    hello c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 /**
  * main - an entry for the prog that prints line to standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
