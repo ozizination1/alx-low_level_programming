9d9aa0a0f83f99dc515d6fe45b1ee406e1bda882
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-20 08:50:47 -0700

    c funtions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - a funtion that prints alphabet 10 times
+ */
+
+
+
+void print_alphabet_x10(void)
+{
+	char za;
+	int i;
+
+	i = 1;
+
+	while (i <= 10)
+	{
+		za = 'a';
+		while (za <= z)
+		{
+			_putchar(za);
+			za++
+		}
+		_putchar('\n')
+		i++
+	}
+}
