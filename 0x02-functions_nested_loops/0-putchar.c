008bfb02d44f19c5d3932caa0c474cebac2eda90
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-20 07:14:05 -0700

    c funtions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - an entry point for the program
+ * Return: always 0 (success)
+ */
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project is on c funtions and nested loops
