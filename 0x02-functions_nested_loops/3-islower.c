2497494b01a447ab216196e6b3c927fcc7ddc116
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-20 14:00:42 -0700

    c funtions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * int _islower(int c)- checks for alphabets in lower case
+ *_islower - checks for alphabets in lower case
  */
 int _islower(int c)
+
 {
-      return(c >= 'a' && c<= 'z');
+	return (c >= 'a' && c <= 'z');
 }
