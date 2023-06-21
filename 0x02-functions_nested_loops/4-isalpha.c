de8119fd1f26d5e8d26cc30ff1ae3114e2cc0a07
ozizination1 <ozizelijahopisa@gmail.com>
2023-06-21 05:27:39 -0700

    c functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,8 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) ||
-	    (c >= 65 && c >= 90))
-		return (1);
-	return (0);
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c >= 'Z'))
+
 }
 
